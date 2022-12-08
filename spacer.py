0be8677bab0b9dcbdd8e8cc135a369241f3c760c
ben <yipjones2@gmail.com>
2022-12-08 20:49:36 +0000

    begun work on multiple spaces

--- spacer.py
+++ spacer.py
@@ -15,6 +15,7 @@ class Iter_item:
 
 class Iterable:
 	iterable = []
+	new_iterable = []
 	longest_string_length = 0
 	margin = 8
 	
@@ -30,7 +31,7 @@ class Iterable:
 		for index,string in enumerate(self.iterable):
 			string_object = Iter_item(string)
 			string_object.spacing  = self.get_spacing(string_object)
-			self.iterable[index] = string_object
+			self.new_iterable.append(string_object)
 
 	def get_spacing(self,string_object):
 		max_spacing = self.longest_string_length + self.margin
@@ -46,15 +47,29 @@ def space(iterable1,iterable2):
 	output = create_output_string(iter1,iter2)
 	return output
 
+
+def make_output_string(iterables):
+	# add spacing for first term, spacing for first item in second arrat, etc
+	string = ""
+	for i in range(len(iterables[0])):
+		for iterable in iterables:
+			string += iterable.new_iterable.make_spaces()
+		string += "\n"
+	return string
+
+
 def space_multiple(iterables):
 	for iterable in iterables:
-		pass
+		#get spacing for each item in the first iterable, and save to new object. 
+		new_iterable = Iterable(iterable)
+		new_iterable.set_spacings()
+	string = make_output_string(iterables)
 
 
 def create_output_string(iter1,iter2):
 	output = ""
-	for index, (item1,item2) in enumerate(zip(iter1.iterable,iter2.iterable)):
-		if index != len(iter1.iterable)-1:
+	for index, (item1,item2) in enumerate(zip(iter1.new_iterable,iter2.new_iterable)):
+		if index != len(iter1.new_iterable)-1:
 			output += item1.string + item1.make_spaces() + item2.string + "\n"
 		else:
 			output += item1.string + item1.make_spaces() + item2.string
@@ -78,6 +93,7 @@ def main():
 		"asd"
 	]
 	print(space(list1, list2))
+	print(space_multiple([list1,list2]))
 
 if __name__ == "__main__":
 	main()
